fdace1f6aa166373344773d3c8335a5a08d7f9bd
strwhl <skillexe92@gmail.com>
2023-04-19 21:58:00 +0300

    add security

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 import os
 
-with open('.env', 'r') as file:
+with open('.env', 'r', 'n') as file:
     line = file.readline()
     try:
         os.environ[line[:line.find("=")]] = line[line.find("=") + 1:]
